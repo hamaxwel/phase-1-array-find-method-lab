37f0d6141ebf4480d9ff40eb0389bbf1fa9c129e
maxwel kirimi <maxwelkirimii@gmail.com>
2024-10-03 13:18:00 +0300

    document structure

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+// index.js
+
+// Function to find the year the Denver Broncos won the Super Bowl
+function superbowlWin(record) {
+    const win = record.find(game => game.result === "W");
+    return win ? win.year : undefined;
+  }
+  
+  // Export the function so it can be imported and tested
+  module.exports = superbowlWin;
